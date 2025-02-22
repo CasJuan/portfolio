8359c82101ecb5494e9e2b44a729d4c095efd5b3
CasJuan <castrojuanig25@gmail.com>
2025-02-22 00:35:48 -0300

    [add] clases para poder centrar toda la pagina, gran mayoria de clases dentro del trabajo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
  export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'oscuro':'#051923',
+        'primario':'#00A6FB',
+        'secondary': '#0582CA',
+        'terciario': '#006494',
+        'cuarto': '#003554'
+      }
+    },
   },
   plugins: [],
 }
